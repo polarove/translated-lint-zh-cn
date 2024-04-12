f26d8ad2b1f311290235ad81016f822d62c975ee
liuqi <liuqi6602@163.com>
2024-04-12 10:58:18 +0800

    构建：@cz-translated-changelog/zh-cn^0.0.41

--- src/informer.ts
+++ src/informer.ts
@@ -7,11 +7,11 @@ export const parseLogMsg = (msg: string, affix?: any) => {
 	return append.concat(affixs).concat(msg)
 }
 
-export const handleBadCommit = (msg: string, affix: string = '❗') => {
+export const handleBadCommit = (msg: string, affix: string = '⚠️') => {
 	console.error(parseLogMsg('未通过提交消息检查', affix))
 	console.log(parseLogMsg(msg, affix))
 }
 
-export const hadnleQualifiedCommit = (msg: string, affix: string = '❤ ') => {
+export const hadnleQualifiedCommit = (msg: string, affix: string = '❤️ ') => {
 	console.log(parseLogMsg(msg, affix))
 }
